c0810cc0a8cef081ecd0fdec0458e57e332f03b6
herbertribeiro19 <hebert11223@gmail.com>
2022-11-04 08:45:03 -0300

    update

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function abrirmenu(){
     let BarraMenuAberto = document.getElementById('barra-menu')
 
     if(BarraMenuAberto.style.width == "0px"){
-        BarraMenuAberto.style.width = "180px";
+        BarraMenuAberto.style.width = "150px";
     }else{
         BarraMenuAberto.style.width = "0px";
     }
