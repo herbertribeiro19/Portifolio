a6a9b549163d5b1851ce31151320a4f962a9e08c
herbertribeiro19 <hebert11223@gmail.com>
2022-11-06 12:01:21 -0300

    javascript update

--- script.js
+++ script.js
@@ -6,4 +6,22 @@ function abrirmenu(){
     }else{
         BarraMenuAberto.style.width = "0px";
     }
+
+    if (BarraMenuAberto.style.height == "0px") {
+        BarraMenuAberto.style.height = "380px";
+    }else{
+        BarraMenuAberto.style.height = "0px";
+    }
+}
+
+function send(){
+    let envioMensagem = document.getElementById('envio')
+
+    if(envioMensagem.onclick){
+        alert("Mensagem enviada com sucesso!")
+        return
+    }else{
+        alert("teste")
+    }
+
 }
