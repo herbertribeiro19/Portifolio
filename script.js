4602ee6b43d79451d42800afc8205e6b335c6c34
herbertribeiro19 <hebert11223@gmail.com>
2022-12-25 18:09:52 -0300

    updated swiper

--- script.js
+++ script.js
@@ -21,5 +21,43 @@ function send(){
         alert("Mensagem enviada com sucesso!");
         return;
     }
-
 }
+
+    //1 var swiper = new Swiper(".swiper-projetos", {
+    //     effect: "cards",
+    //     grabCursor: true,
+    //   });
+
+    //2 var swiper = new Swiper(".swiper-projetos", {
+    //     slidesPerView: 1,
+    //     spaceBetween: 0,
+    //     loop: false,
+    //     //grabCursor: true,
+    //     // pagination: {
+    //     //     el: ".swiper-pagination",
+    //     //     clickable: true,
+    //     // },
+    //     // navigation: {
+    //     //     nextEl: ".swiper-button-next",
+    //     //     prevEl: ".swiper-button-prev",
+    //     // },
+    // });
+
+    var swiper = new Swiper(".swiper-projetos", {
+        effect: "coverflow",
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: 1.7,
+        loop: true,
+        coverflowEffect: {
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+        },
+    });
+
