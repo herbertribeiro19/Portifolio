9be8dcf3584f81574c3fcfb01473bbea662b1a69
herbertribeiro19 <hebert11223@gmail.com>
2022-12-26 11:52:33 -0300

    update responsive

--- script.js
+++ script.js
@@ -47,17 +47,33 @@ function send(){
         effect: "coverflow",
         grabCursor: true,
         centeredSlides: true,
-        slidesPerView: 1.7,
+        slidesPerView: 1,
         loop: true,
         coverflowEffect: {
-            rotate: 50,
+            rotate: 20,
             stretch: 0,
             depth: 100,
             modifier: 1,
-            slideShadows: true,
+            slideShadows: false,
+        },
+        breakpoints: {
+            320: {
+                slidesPerView: 1,
+                centeredSlides: false,
+            },
+            768: {
+                slidesPerView: 0,
+            },
+            991: {
+                slidesPerView: 2,
+            },
+            1200: {
+                slidesPerView: 0,
+            },
         },
         pagination: {
             el: ".swiper-pagination",
         },
+
     });
 
